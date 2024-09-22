effefd05935cd29e48d2189123927cc01bce2e03
Ronit Bose <rbose7997@gmail.com>
2024-09-22 17:07:36 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,78 @@
-import Image from "next/image";
+"use client";
+
+import useUser from '@/hooks/useUser';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { v4 as uuid } from "uuid";
+
+
 
 export default function Home() {
+  const {fullName, setFullName} = useUser();
+  const [roomID, setRoomID] = useState("");
+  const router = useRouter();
+  useEffect (() => {
+    setFullName("");
+  }, []);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="w-full h-screen">
+    <section className="bg-gray-950 text-white ">
+        <div className="mx-auto max-w-screen-xl px-4 py-32 flex-col gap-24 flex h-screen items-center">
+          <Image src="/logo.svg" alt="logo" width={200} height={200} />
+          <div className="mx-auto max-w-4xl text-center ">
+            {/* <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text">
+              {'Have a smooth meeting'}
+            </h1> */}
+            {/* <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text">
+              <span className="Block">With team members</span>
+            </h1> */}
+            <p className="mx-auto mt-6 max-w-xl sm:text-xl/relaxed">
+            Our In-App Voice & Video chat application enables seamless one to one interactive voice & video chat directly with your consultant. Whether you&apos;re opting for your first session, evaluation of reports or monthly check-ups over the air it&apos;s an all in one solution.
+            </p>
+            <div className="flex items-center justify-center gap-4 mt-6">
+              <input
+                type="text"
+                id="name"
+                onChange={(e) => setFullName(e.target.value.toString())}
+                className="border rounded-md focus:border-transparent focus:outline-none focus:ring-0 px-4 py-2 w-full text-black"
+                placeholder="Enter your name"
+                style={{ marginBottom: '10px' }}
+              />
+            </div>
+            {fullName && fullName.length >= 3 && (
+            <>
+              <div>
+                <input
+                  type="text"
+                  id="roomid"
+                  value={roomID}
+                  onChange={(e) => setRoomID(e.target.value.toString())}
+                  className="border rounded-md focus:border-transparent focus:outline-none focus:ring-0 px-4 py-2 w-full text-black"
+                  placeholder="Enter room id to join a meeting"
+                  style={{ marginBottom: '10px' }}
+                />
+                <button
+                      className="rounded-md bg-blue-600 px-10 py-[11px] text-sm font-medium text-white focus:outline-none sm:w-auto"
+                      onClick={() => router.push(`/room/${roomID}`)}
+                      disabled={!roomID}
+                    >
+                      Join
+                </button>
+              </div>
+              <div className="mt-4 flex items-center justify-center">
+                  <button
+                    className="text-lg font-medium hover:text-blue-400 hover:underline"
+                    onClick={() => router.push(`/room/${uuid()}`)}
+                  >
+                    Or create a new meeting
+                  </button>
+              </div>
+            </>
+            )}
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </section>
+  </div>
   );
 }
